0b5814415375f683fb5770f0b430cb142be2e6c3
ababs17 <116195403+ababs17@users.noreply.github.com>
2024-09-05 11:59:07 -0700

    contract.ts

--- src/contract.ts
+++ src/contract.ts
@@ -1,31 +1,31 @@
-import {
-  OwnerSet as OwnerSetEvent,
-  ImplementationSet as ImplementationSetEvent
-} from "../generated/Contract/Contract"
-import { OwnerSet, ImplementationSet } from "../generated/schema"
+
+import { OwnerSet as OwnerSetEvent, ImplementationSet as ImplementationSetEvent } from "../generated/Contract/Contract";
+import { OwnerEntity, ImplementationEntity, Transaction } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 
 export function handleOwnerSet(event: OwnerSetEvent): void {
-  let entity = new OwnerSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
+  let transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.blockNumber = event.block.number;
+  transaction.timestamp = event.block.timestamp;
+  transaction.owner = event.params.newOwner.toHex();
+  transaction.save();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let ownerSet = new OwnerEntity(event.transaction.hash.toHex());
+  ownerSet.newOwner = event.params.newOwner.toHex();
+  ownerSet.transaction = transaction.id;
+  ownerSet.save();
 }
 
 export function handleImplementationSet(event: ImplementationSetEvent): void {
-  let entity = new ImplementationSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
+  let transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.blockNumber = event.block.number;
+  transaction.timestamp = event.block.timestamp;
+  transaction.owner = event.params.newImplementation.toHex();
+  transaction.save();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let implementationSet = new ImplementationEntity(event.transaction.hash.toHex());
+  implementationSet.newImplementation = event.params.newImplementation.toHex();
+  implementationSet.transaction = transaction.id;
+  implementationSet.save();
 }
+
